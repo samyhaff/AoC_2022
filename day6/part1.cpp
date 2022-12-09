075c7223cddf1e7cde77cdb7c1c8877c0b3df967
samyhaff <haffoudhisamy@gmail.com>
2022-12-09 18:56:18 +0100

    solved day 6 part 1

---
+++ day6/part1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define LENGTH 4
+
+int main() {
+    string input;
+    unordered_set<char> set;
+
+    getline(cin, input);
+    for (int i = LENGTH; i < input.length(); i++) {
+        set.clear();
+        for (int j = i - LENGTH; j < i; j++)
+            set.insert(input[j]);
+        if (set.size() == LENGTH) {
+            cout << i << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
