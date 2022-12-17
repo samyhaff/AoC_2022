7e82f8c4192b887d9ec89bab07407bb256f7cdcb
samyhaff <haffoudhisamy@gmail.com>
2022-12-17 10:51:53 +0100

    works on example input

--- day13/part1.cpp
+++ day13/part1.cpp
@@ -2,16 +2,17 @@
 
 using namespace std;
 
-vector<string> get_values(string list) {
-    vector<string> values;
+pair<string, string> get_value_and_rest(string list) {
+    string value, rest;
     int brackets_count = 0;
     int i = 1;
 
     for (int j = 1; j < list.size(); j++) {
         if ((list[j] == ',' || list[j] == ']') && brackets_count == 0) {
-            values.push_back(list.substr(i, j - i));
-            cout << list.substr(i, j - i) << " ";
-            i = j + 1;
+            value = list.substr(i, j - i);
+            rest = list.substr(j + 1, list.size() - j - 1);
+            rest = rest.empty() ? "" : rest.insert(0, "[");
+            break;
         }
         else if (list[j] == ']')
             brackets_count--;
@@ -19,29 +20,47 @@ vector<string> get_values(string list) {
             brackets_count++;
     }
 
-    cout << endl;
-    return values;
+    return {value, rest};
 }
 
-// bool compare(string a, string b) {
-//     if (a[0] != '[' && b[0] != '[')
-//         return stoi(a) <= stoi(b);
-//     else if (a[0] != '[' && b[0] == '[')
-//         return compare('[' + a + ']', b);
-//     else if (a[0] == '[' && b[0] != '[')
-//         return compare(a, '[' + b + ']');
-//     else {
-//
-//     }
-// }
+bool compare(string a, string b) {
+    pair<string, string> pair_a = get_value_and_rest(a);
+    pair<string, string> pair_b = get_value_and_rest(b);
+    string value_a = pair_a.first;
+    string value_b = pair_b.first;
+    string rest_a = pair_a.second;
+    string rest_b = pair_b.second;
+
+    if (value_a.empty()) return true;
+    if (value_b.empty()) return false;
+    if (value_a[0] != '[' && value_b[0] != '[') {
+        if (stoi(value_a) < stoi(value_b)) return true;
+        else if (stoi(value_a) > stoi(value_b)) return false;
+        else return compare(rest_a, rest_b);
+    }
+    if (value_a[0] == '[' && value_b[0] == '[' && !compare(value_a, value_b))
+        return false;
+    else if (value_a[0] == '[' && value_b[0] != '[' && !compare(value_a, "[" + value_b + "]"))
+        return false;
+    else if (value_a[0] != '[' && value_b[0] == '[' && !compare("[" + value_a + "]", value_b))
+        return false;
+
+    return compare(rest_a, rest_b);
+}
 
 int main() {
     string a, b;
+    int ans = 0;
+    int i = 1;
 
     while (cin >> a >> b) {
-        get_values(a);
-        get_values(b);
+        if (compare(a, b)) {
+            ans += i;
+        }
+        i++;
     }
+
+    cout << ans << endl;
 
     return 0;
 }
