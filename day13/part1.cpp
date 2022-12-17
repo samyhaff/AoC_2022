1729b4da6188de18c7849c28e98ef35212914def
samyhaff <haffoudhisamy@gmail.com>
2022-12-17 14:34:16 +0100

    FINALLY working day 13 part 1

--- day13/part1.cpp
+++ day13/part1.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+enum cmp { SMALLER, EQUAL, BIGGER };
+
 pair<string, string> get_value_and_rest(string list) {
     string value, rest;
     int brackets_count = 0;
@@ -23,40 +25,43 @@ pair<string, string> get_value_and_rest(string list) {
     return {value, rest};
 }
 
-bool compare(string a, string b) {
+cmp compare(string a, string b) {
     pair<string, string> pair_a = get_value_and_rest(a);
     pair<string, string> pair_b = get_value_and_rest(b);
     string value_a = pair_a.first;
     string value_b = pair_b.first;
     string rest_a = pair_a.second;
     string rest_b = pair_b.second;
+    cmp res;
 
-    if (value_a.empty()) return true;
-    if (value_b.empty()) return false;
-    if (value_a[0] != '[' && value_b[0] != '[') {
-        if (stoi(value_a) < stoi(value_b)) return true;
-        else if (stoi(value_a) > stoi(value_b)) return false;
+    if (value_a.empty() && !value_b.empty()) return SMALLER;
+    else if (value_b.empty() && !value_a.empty()) return BIGGER;
+    else if (value_a.empty() && value_b.empty()) return EQUAL;
+    else if (value_a[0] != '[' && value_b[0] != '[') {
+        if (stoi(value_a) < stoi(value_b)) return SMALLER;
+        else if (stoi(value_a) > stoi(value_b)) return BIGGER;
+        else if (rest_a.empty() && rest_b.empty()) return EQUAL;
         else return compare(rest_a, rest_b);
     }
-    if (value_a[0] == '[' && value_b[0] == '[' && !compare(value_a, value_b))
-        return false;
-    else if (value_a[0] == '[' && value_b[0] != '[' && !compare(value_a, "[" + value_b + "]"))
-        return false;
-    else if (value_a[0] != '[' && value_b[0] == '[' && !compare("[" + value_a + "]", value_b))
-        return false;
+    else if (value_a[0] == '[' && value_b[0] == '[')
+        res = compare(value_a, value_b);
+    else if (value_a[0] == '[' && value_b[0] != '[')
+        res = compare(value_a, "[" + value_b + "]");
+    else if (value_a[0] != '[' && value_b[0] == '[')
+        res = compare("[" + value_a + "]", value_b);
 
-    return compare(rest_a, rest_b);
+    return (res != EQUAL) ? res : compare(rest_a, rest_b);
 }
 
 int main() {
     string a, b;
     int ans = 0;
     int i = 1;
+    cmp res;
 
     while (cin >> a >> b) {
-        if (compare(a, b)) {
-            ans += i;
-        }
+        res = compare(a, b);
+        if (res != BIGGER) ans += i;
         i++;
     }
 
