38c8889195133764d124bd948253d2729be386b8
samyhaff <haffoudhisamy@gmail.com>
2022-12-18 11:44:59 +0100

    fixed parser and added get_limits

--- day14/part1.cpp
+++ day14/part1.cpp
@@ -10,7 +10,7 @@ vector<vector<pair<int, int>>> read_input() {
 
     while (getline(cin, line)) {
         lines.push_back(vector<pair<int,int>>());
-        ss.str(line);
+        ss.clear(); ss.str(line);
         while (ss >> word) {
             sscanf(word.c_str(), "%d,%d", &coordinates.first, &coordinates.second);
             if (ss.peek() != EOF)
@@ -22,8 +22,23 @@ vector<vector<pair<int, int>>> read_input() {
     return lines;
 }
 
+pair<int, int> find_limits(const vector<vector<pair<int, int>>>& lines) {
+    pair<int, int> limits = { 0, 0 };
+    for (auto& line: lines) {
+        for (auto& coordinates: line) {
+            if (coordinates.first > limits.first) limits.first = coordinates.first;
+            if (coordinates.second > limits.second) limits.second = coordinates.second;
+        }
+    }
+
+    return limits;
+}
+
 int main() {
     vector<vector<pair<int, int>>> lines = read_input();
+    pair<int, int> limits = find_limits(lines);
+
+    cout << limits.first << " " << limits.second << endl;
 
     return 0;
 }
