b42f2b76cf581e1ee0690f9b1314385fe480851d
samyhaff <haffoudhisamy@gmail.com>
2022-12-19 18:08:30 +0100

    solved day 14 part 2

---
+++ day14/part2.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const pair<int, int> falling_point = { 500, 0 };
+const int offset = 2;
+
+vector<vector<pair<int, int>>> read_input() {
+    vector<vector<pair<int, int>>> lines;
+    string line, word;
+    pair<int, int> coordinates;
+    stringstream ss;
+
+    while (getline(cin, line)) {
+        lines.push_back(vector<pair<int,int>>());
+        ss.clear(); ss.str(line);
+        while (ss >> word) {
+            sscanf(word.c_str(), "%d,%d", &coordinates.first, &coordinates.second);
+            if (ss.peek() != EOF)
+                ss >> word;
+            lines.back().push_back(coordinates);
+        }
+    }
+
+    return lines;
+}
+
+pair<int, int> find_limits(const vector<vector<pair<int, int>>>& lines) {
+    pair<int, int> limits = { 0, 0 };
+    for (auto& line: lines) {
+        for (auto& coordinates: line) {
+            if (coordinates.second > limits.second) limits.second = coordinates.second;
+        }
+    }
+
+    limits.second += offset;
+    limits.first = falling_point.first + limits.second;
+
+    return limits;
+}
+
+vector<vector<char>> make_map(const vector<vector<pair<int, int>>>& lines) {
+    auto limits = find_limits(lines);
+    vector<vector<char>> map(limits.second + 1, vector<char>(limits.first + 1, '.'));
+    pair<int, int> direction, coordinates;
+
+    map[map.size() - 1] = vector<char>(map[0].size(), '#');
+
+    for (const auto& line: lines) {
+        for (int i = 0; i < line.size() - 1; i++) {
+            direction = (line[i].first == line[i + 1].first)
+                ? (line[i].second < line[i + 1].second ? make_pair(0, 1)
+                                                       : make_pair(0, -1))
+                : (line[i].first < line[i + 1].first ? make_pair(1, 0)
+                                                     : make_pair(-1, 0));
+            coordinates = line[i];
+            map[coordinates.second][coordinates.first] = '#';
+            while (coordinates != line[i + 1]) {
+                coordinates = { coordinates.first + direction.first,
+                                coordinates.second + direction.second };
+                map[coordinates.second][coordinates.first] = '#';
+            }
+        }
+    }
+
+    return map;
+}
+
+bool simulate_falling_sand(vector<vector<char>>& map) {
+    pair<int, int> pos = falling_point;
+    int x_lim = map[0].size(), y_lim = map.size();
+
+
+    while (true) {
+        if (map[pos.second + 1][pos.first] == '.') {
+            pos.second++;
+        } else if (map[pos.second + 1][pos.first - 1] == '.') {
+            pos.first--;
+            pos.second++;
+        } else if (map[pos.second + 1][pos.first + 1] == '.') {
+            pos.first++;
+            pos.second++;
+        } else {
+            map[pos.second][pos.first] = 'o';
+            return (pos == falling_point) ? false : true;
+        }
+    }
+}
+
+int solve(vector<vector<char>>& map) {
+    int ans = 1;
+    while (simulate_falling_sand(map)) ans++;
+    return ans;
+}
+
+void print_map(const vector<vector<char>>& map) {
+    for (int i = 0; i < map.size(); i++) {
+        for (int j = 0; j < map[0].size(); j++)
+            cout << map[i][j] << " ";
+        cout << endl;
+    }
+}
+
+int main() {
+    vector<vector<pair<int, int>>> lines = read_input();
+    vector<vector<char>> map = make_map(lines);
+    int ans = solve(map);
+
+    cout << ans << endl;
+
+    return 0;
+}
